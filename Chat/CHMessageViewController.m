5a7a624e31c3f055a210d41a990e12ef517bf115
Ethan Mick <ethanmski@gmail.com>
2014-11-22 23:30:21 -0500

    Removed log statement.

--- Chat/CHMessageViewController.m
+++ Chat/CHMessageViewController.m
@@ -854,7 +854,6 @@ NSString *const CHRefreshCellIdentifier = @"CHRefreshCellIdentifier";
 /*
 - (BOOL)canPerformAction:(SEL)action withSender:(id)sender;
 {
-    NSLog(@"Action? %@ Sender? %@", NSStringFromSelector(action), sender);
     UIPasteboard *gpBoard = [UIPasteboard generalPasteboard];
     
     if ([NSStringFromSelector(action) isEqualToString:@"paste:"] && gpBoard.image) { //add more types later.
